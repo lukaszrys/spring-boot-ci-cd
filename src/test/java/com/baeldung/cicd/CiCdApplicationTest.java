559447ae3b3462f86d79e361689fcd8cb0bb798f
Lukasz Rys <lukaszrys@me.com>
2020-04-27 12:42:58 +0200

    Add test

--- src/test/java/com/baeldung/cicd/CiCdApplicationTest.java
+++ src/test/java/com/baeldung/cicd/CiCdApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CiCdApplicationTest {
+public class CiCdApplicationTest {
 
     @Test
     public void contextLoads() {
